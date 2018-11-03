96f444ee0c8ae5fb5eed8d28ed5297c97a99f9c6
dhruv26patel <dhruv26patel@gmail.com>
2018-11-03 14:15:30 -0500

    updated Jenkinsfile

--- fetchData.py
+++ fetchData.py
@@ -3,7 +3,12 @@
 #response = requests.get("http://api.open-notify.org/iss-pass.json")
 #print(response.status_code)
 
-print "Hello"
-print (help('modules') )
+#print "Hello"
+#print (help('modules') )
 
 
+import urllib
+
+req = urllib.request.Request('http://www.voidspace.org.uk')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
